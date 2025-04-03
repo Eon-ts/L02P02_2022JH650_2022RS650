8eab3a4f2c13c24a13bd03b2ffb60dfad45902a4
Eon-ts <aeonsitux@gmail.com>
2025-04-03 17:52:22 -0600

    Agregar archivos de proyecto.

---
+++ L02P02_2022JH650_2022RS650/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace L02P02_2022JH650_2022RS650.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
